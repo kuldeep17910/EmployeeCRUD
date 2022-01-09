9220e15b7c038cb3e9dfc532c97059f974857d9f
kuldeep17910 <kuldeep17910@gmail.com>
2022-01-09 17:58:56 +0530

    Add project files.

---
+++ EmployeeCRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeCRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeCRUD.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeCRUD/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeCRUD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name ="Employee Name")]
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Address { get; set; }        
+        public DateTime? RecordCreatedOn { get; set; }
+
+    }
+}
